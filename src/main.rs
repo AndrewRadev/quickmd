8b5bc812648a158dc298aa243aeed4dbc6ba744d
Andrew Radev <andrey.radev@gmail.com>
2019-06-09 21:49:45 +0300

    Render given HTML file

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,38 @@
 use gtk::prelude::*;
 use gtk::{Window, WindowType};
 use webkit2gtk::{WebContext, WebView, WebViewExt};
+use std::env;
+use std::path::Path;
 
 fn main() {
     if gtk::init().is_err() {
-        println!("Failed to initialize GTK.");
+        eprintln!("Failed to initialize GTK.");
         return;
     }
 
+    let argument = match env::args().nth(1) {
+        Some(f) => f,
+        None => {
+            eprintln!("USAGE: quickmd <file.md>");
+            return;
+        },
+    };
+    let path = match Path::new(&argument).canonicalize() {
+        Ok(p) => p,
+        Err(e) => {
+            eprintln!("Error reading {}: {}", argument, e);
+            return
+        },
+    };
+
     let window = Window::new(WindowType::Toplevel);
-    window.set_title("Quickmd: TODO document title");
+    window.set_title(&format!("Quickmd: {}", path.display()));
     //window.set_default_size(350, 70);
 
     // Create a the WebView for the preview pane.
     let context = WebContext::get_default().unwrap();
     let preview = WebView::new_with_context(&context);
-    preview.load_html("<h1>Test <u>Underlined</u></h1>", Some("file://example.html"));
+    preview.load_uri(&format!("file://{}", path.display()));
 
     window.add(&preview);
     window.show_all();
