4c326cf97450df5b7f10e2f081407817aa3ea37b
Andrew Radev <andrey.radev@gmail.com>
2019-06-14 21:48:51 +0300

    Parse markdown into HTML

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+use std::process::exit;
+
 use gtk::prelude::*;
 use gtk::{Window, WindowType};
 use webkit2gtk::{WebContext, WebView, WebViewExt};
-use std::env;
-use std::path::Path;
+use pulldown_cmark::{Parser, html};
 
 fn main() {
     if gtk::init().is_err() {
         eprintln!("Failed to initialize GTK.");
-        return;
+        exit(1);
     }
 
     let argument = match env::args().nth(1) {
         Some(f) => f,
         None => {
             eprintln!("USAGE: quickmd <file.md>");
-            return;
+            exit(1);
         },
     };
     let path = match Path::new(&argument).canonicalize() {
         Ok(p) => p,
         Err(e) => {
             eprintln!("Error reading {}: {}", argument, e);
-            return
+            exit(1);
         },
     };
 
@@ -29,10 +33,23 @@ fn main() {
     window.set_title(&format!("Quickmd: {}", path.display()));
     //window.set_default_size(350, 70);
 
+    let markdown = match fs::read_to_string(&path) {
+        Ok(s) => s,
+        Err(_) => {
+            eprintln!("Couldn't read markdown file: {}", path.display());
+            exit(1);
+        },
+    };
+
+    // Parse markdown
+    let parser = Parser::new(&markdown);
+    let mut html_output = String::new();
+    html::push_html(&mut html_output, parser);
+
     // Create a the WebView for the preview pane.
     let context = WebContext::get_default().unwrap();
     let preview = WebView::new_with_context(&context);
-    preview.load_uri(&format!("file://{}", path.display()));
+    preview.load_html(&html_output, None);
 
     window.add(&preview);
     window.show_all();
