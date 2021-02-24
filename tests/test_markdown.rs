f973a3865a8862b8736c07f0aa363f9a2275a51c
Andrew Radev <andrey.radev@gmail.com>
2021-02-24 17:52:25 +0200

    Image rendering test

--- tests/test_markdown.rs
+++ tests/test_markdown.rs
@@ -28,3 +28,23 @@ fn test_keeps_track_of_rendered_languages() {
 
     assert_eq!(expected, content.code_languages);
 }
+
+#[test]
+fn test_renders_local_images() {
+    let mut file = NamedTempFile::new().unwrap();
+    let tempdir = file.path().parent().unwrap().to_path_buf();
+
+    writeln!(file, "![demo image](./local-image.png)").unwrap();
+    writeln!(file, "![demo image](http://remote-image.png)").unwrap();
+    writeln!(file, "![demo image](unprefixed_image.png)").unwrap();
+
+    let renderer = Renderer::new(file.path().to_path_buf());
+    let content = renderer.run().unwrap();
+
+    let local_src = format!("src=\"file://{}/local-image.png\"", tempdir.display());
+    assert!(content.html.contains(&local_src));
+    assert!(content.html.contains("src=\"http://remote-image.png\""));
+
+    // Without a ./ prefix, it's left alone
+    assert!(content.html.contains("src=\"unprefixed_image.png\""));
+}
